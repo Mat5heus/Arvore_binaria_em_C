10dd2d0c4f47602d3c4dd28854934db1112b8e79
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2020-11-10 16:13:41 -0300

    'implementacao de mensagens (em uma funcao unica), inserir (nao esta pegando), emOrdem, lerDoUsuario e printarRaiz'

--- func.c
+++ func.c
@@ -1,25 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "arvore.h"
 
 // Gera as constantes true e false
 #define true 1
 #define false 0
 
+void mensagens(int cod, int valor) {
+    printf("\nERRO %d: ", cod);
+    switch (cod) {
+    case 1:
+        printf("O valor %d ja foi adicionado\n", valor);
+        break;
+    default:
+        printf("Mensagem nao encontrada!");
+        break;
+    }
+}
+
 void iniciarArvore(Arvore **tree) { // Inicializa a arvore com valor "vazio"
     *tree = NULL;
 }
 
-void inserirNo(Arvore **tree, int valor) {}
+//Não esta preenchendo a arvore corretamente
+void inserirNo(Arvore **tree, int valor) {
+    Arvore *aux = *tree;
+
+    if(valor == NULL) return;
+
+    if(estaVazia(*tree)) {
+        Arvore *novo;
+
+        novo = (Arvore*) malloc(sizeof(Arvore));
+        
+        novo->pai = NULL;
+        novo->dir = NULL;
+        novo->esq = NULL;
+        novo->dado = valor;
+
+        *tree = novo;
+    } else {
+        if(aux->dado == valor)
+            mensagens(1, valor);
+        else if(aux->dado > valor)
+            inserirNo(&aux->esq, valor);
+        else
+            inserirNo(&aux->dir, valor);
+    }
+}
 
 void lerArquivo(Arvore **tree) {}
 
-void lerDoUsuario(Arvore **tree) {}
+void lerDoUsuario(Arvore *tree) {
+    int resp = NULL;
 
-int estaVazia(Arvore *tree) {}
+    printf(
+        "\nDigite -1 para finalizar a leitura...\n"
+        "\nDigite o(s) valor(es) que deseja adicionar: "
+    );
+    do {
+        inserirNo(&tree, resp);
+        scanf("%d", &resp);
+    } while(resp != -1);
+}
+
+int estaVazia(Arvore *tree) {
+    if(tree == NULL)
+        return true;
+    else
+        return false;
+}
 
 Arvore* buscar(Arvore *tree,int valor) {}
 
-void printarNoRaiz(Arvore *tree) {}
+int printarNoRaiz(Arvore *tree) {
+    Arvore *aux = tree;
+    return aux->dado;
+}
 
 void printarNosFolha(Arvore *tree) {}
 
@@ -43,4 +100,10 @@ void preOrdem(Arvore *tree) {} //Gui
 
 void posOrdem(Arvore *tree) {} //Gui
 
-void emOrdem(Arvore *tree) {} //Gui
+void emOrdem(Arvore *tree) {
+    if (!estaVazia(tree)) {
+        emOrdem(tree->esq);
+        printf("%d ", tree->dado);
+        emOrdem(tree->dir);
+    }
+} 
